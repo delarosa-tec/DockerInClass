3752f3cfdd52bbf059d85b1b574656d9dd595f95
Jorge De la Rosa <delarosa@tec.mx>
2022-11-15 06:30:37 -0400

    swagger

--- SegundaApi/index.js
+++ SegundaApi/index.js
@@ -2,6 +2,28 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'segunda-api' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new winston.transports.Console({
+//     format: winston.format.simple(),
+//   }));
+// }
+
 const app = express()
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -20,13 +42,29 @@ app.get('/health', function (req, res) {
 //El metodo de busqueda de usuarios
 app.get('/users/:id', function (req, res) {
   var variable1 = req.params.id;
-  res.send('Respuesta: ' + variable1)
+  //TODO: Crear busqueda de usuario
+  if(variable1 == "admin") {
+    const response = {nombre: 'John', apellido: 'Doe', propiedades: { edad: '49'}} 
+    logger.info('Se devuelve la respuesta ' + JSON.stringify(response))
+    logger.warn('Muestra Warn')
+    logger.warn('Muestra Request: ' + variable1)
+    res.json(response)
+    // res.send('Usuario '+ variable1 + ' encontrado en la base de datos.')
+  }
+  else {
+    const responseError = 'Usuario inexistente, favor de validar.'
+    logger.error(responseError + ' Usuario: ' + variable1)
+    res.status(404).send(responseError)
+  }
 })
 
 //Insertar Usuario
-app.post('/users', function (request, response) {
+app.post('/users', function (request, resp) {
   var variable1 = request.body;
-  response.send('Respuesta: ' + variable1.nombre + ' ' + variable1.apellido)
+  const response = 'Respuesta: ' + variable1.nombre + ' ' + variable1.apellido
+  // console.log('Respuesta: ' + variable1.nombre + ' ' + variable1.apellido)
+    logger.info(response)
+    resp.send(response)
 })
 
 app.listen(3000)
